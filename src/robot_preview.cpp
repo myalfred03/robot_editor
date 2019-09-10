939a4f8726bffbb9703b45d2390f2f69b0992cf9
Yesser-Morales95 <moralesalfredo133@gmail.com>
2019-09-09 20:39:10 -0600

    LVR Final

--- src/robot_preview.cpp
+++ src/robot_preview.cpp
@@ -4,7 +4,9 @@
 #include <rviz/visualization_manager.h>
 #include <rviz/render_panel.h>
 #include <rviz/display.h>
-
+#include <rviz/yaml_config_reader.h>
+#include <rviz/yaml_config_writer.h>
+#include <ros/package.h>
 #include "robot_preview.h"
 
 RobotPreview::RobotPreview(QWidget* parent) :
@@ -24,11 +26,30 @@ RobotPreview::RobotPreview(QWidget* parent) :
 	// initialize the main RViz Classes
 	manager_ = new rviz::VisualizationManager(render_panel_);
 	render_panel_->initialize(manager_->getSceneManager(), manager_);
+
+	rviz::YamlConfigReader reader;
+	rviz::Config config;
+	std::string filename = ros::package::getPath("rvizglabre")+"/config/lvr.rviz" ;
+	reader.readFile( config, QString::fromStdString( filename ));
+	  if( !reader.error() )
+	  {
+	      manager_->load( config.mapGetChild( "Visualization Manager" ));
+	  }
+
 	manager_->initialize();
 	manager_->startUpdate();
 
 	// Create a grid display
-	grid_ = manager_->createDisplay("rviz/Grid", "Robot Preview", true);
+   grid_ = manager_->createDisplay("rviz/Grid", "Robot Preview", true);
+   grid_->subProp( "Plane Cell Count" )->setValue( 30 );
+   grid_->subProp( "Cell Size" )->setValue( 1 );
+  // grid_->subProp( "Line Style" )->setValue( "Billboards" );
+   //grid_->subProp( "Color" )->setValue( QColor( Qt::black ) );
+   grid_->subProp( "Color" )->setValue( QColor(0, 0, 0));
+
+
+
+
   tF_   = manager_->createDisplay( "rviz/TF","TF", true); // active mode or disable at load model robot
   tF_->subProp( "Show Names" )->setValue(false);
   tF_->subProp( "Show Axes" )->setValue(true);
